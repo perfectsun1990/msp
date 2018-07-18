e0c9c7e4efc25b0cd3f187feb6b8cfbc5ef9552e
sunfp <perfectsun1990@163.com>
2018-07-18 15:11:40 +0800

    一个经典定长内存池的实现

--- sub_module/mempool/mempool.cpp
+++ sub_module/mempool/mempool.cpp
@@ -1,7 +1,57 @@
 ﻿
 #include "mempool.hpp"
-#include <iostream>
+#include <vector>
 
-int main() {
+struct OBJ_1
+{
+	OBJ_1() {
+		std::cout << "OBJ_1()" << std::endl;
+	}
+	OBJ_1(int a) :value(a) {
+		std::cout << "OBJ_1(int a)" << std::endl;
+	}
+	int value = 0;
+};
+
+struct OBJ_2 
+{
+	int a;
+	int b;
+};
+int main()
+{
+	MemPool<OBJ_2> mpool;// ok
+
+	std::vector<OBJ_1*> obj_table;
+	MemPool<OBJ_1> *pool = new MemPool<OBJ_1>(2);
+
+#if 1
+#define TEST_OBJ_NUMS 8
+	OBJ_1* obj_tmp = nullptr;
+	for (int i = 0; i < TEST_OBJ_NUMS; i++){
+		obj_tmp = pool->New(i);
+		std::cout << "create : " << obj_tmp << std::endl;
+		obj_table.push_back(obj_tmp);
+	}
+	std::cout << "===============================================" << std::endl;
+	for (int i = 0; i < TEST_OBJ_NUMS/2; i++){
+		obj_tmp = obj_table.back();
+		std::cout << "destroy : " << obj_tmp << std::endl;
+		pool->Delete(obj_tmp);
+		obj_table.pop_back();
+	}
+	std::cout << "===============================================" << std::endl;
+	for (int i = 0; i < TEST_OBJ_NUMS/2; i++){
+		std::cout << "CREAT SUCCESSFUL" << std::endl;
+		obj_tmp = pool->New();
+		std::cout << "creat : " << obj_tmp << std::endl;
+		obj_table.push_back(obj_tmp);
+	}
+	std::cout << "===============================================" << std::endl;
+	std::cout << obj_table.size() << std::endl;
+	delete pool;
+#endif
+
+	system("pause");
 	return 0;
 }
--- sub_module/mempool/mempool.hpp
+++ sub_module/mempool/mempool.hpp
@@ -1,3 +1,97 @@
-﻿
+﻿/************************************************************************/
+/* 内存池（Memory Pool），是内存分配器（Memory Allocation）的一种表现形式。
+   它以预存储的方式预先分配一大块内存（相对于每次请求的内存大小来说），
+   使得绝大部分的内存请求只需要在已分配的大块内存上划分出一小块来即可。                                                                     */
+/************************************************************************/
+
 #pragma once
 
+#include <iostream>
+
+template<class T> class MemPool
+{
+	struct MemNode
+	{//作为内存分配节点存在，每个节点默认管理32*sizeof(T)的内存。
+		void* _memPoint; //内存指针
+		MemNode* _next;  //下一节点，和当前节点拥有一样的 对象大小*对象个数 的内存块。
+		size_t _itemNum; //对象个数
+
+		MemNode(size_t itemNum) : _itemNum(itemNum), _next(NULL) {
+			_memPoint = malloc(itemSize() * _itemNum);
+		}
+		~MemNode() {
+			free(_memPoint);
+			_memPoint = NULL;
+			_next = NULL;
+		}
+	};
+protected:
+	size_t _countIn;
+	MemNode* _first;
+	MemNode* _last;
+	size_t _maxNum;			//the max size for one block
+	T* _lastDelete;
+protected:
+	static inline size_t itemSize() {
+		static size_t _itemSize = (sizeof(T) > sizeof(void*)) ? sizeof(T) : sizeof(void*);
+		std::cout << _itemSize << std::endl;
+		return _itemSize;
+	}
+	size_t _GetMemSize()
+	{
+		return (_last->_itemNum * 2 > _maxNum) ? _maxNum : _last->_itemNum * 2;
+	}
+public:
+	MemPool(size_t initNum = 32, size_t maxNum = 10000)
+		: _maxNum(maxNum), _countIn(0), _lastDelete(nullptr)
+	{
+		_first = _last = new MemNode(initNum);
+		std::cout << "MemPool() initNum=" << initNum << std::endl;
+	}
+	~MemPool()
+	{
+		std::cout << "~MemPool()" << std::endl;
+		MemNode* cur = _first;
+		while (cur)
+		{
+			MemNode* del = cur;
+			cur = cur->_next;
+			delete del;
+		}
+		_first = _last = NULL;
+		if (_lastDelete)
+			std::cout << "Warning: left destroy memory and not used" << std::endl;
+		_lastDelete = NULL;
+	}
+
+	template<typename... Args> T* New(Args&&... args)
+	{
+		T* obj = NULL;
+		if (_lastDelete) {
+			obj = _lastDelete;
+			_lastDelete = *(T**)(_lastDelete);//获取下一个空闲的T*指针。
+			return new(obj)T;
+		}else {
+			if (_countIn >= _last->_itemNum)
+			{
+				MemNode* tmp = new MemNode(_GetMemSize());
+				_last->_next = tmp;
+				_last = tmp;
+				_countIn = 0;
+			}
+			obj = (T*)((char*)_last->_memPoint + itemSize()*_countIn);
+			_countIn++;
+			return new(obj)T(std::forward<Args>(args)...);
+		}
+	}
+
+	void Delete(T* ptr)
+	{
+		if (nullptr != ptr)
+		{//注意：将_lastDelete转换成T**，再通过*取引用后获取的就是T*而非原先的T类型变量，这时就可以存储T*类型的指针了。
+			ptr->~T();
+			*(T**)ptr = _lastDelete;
+			_lastDelete = ptr;
+		}
+	}
+};
